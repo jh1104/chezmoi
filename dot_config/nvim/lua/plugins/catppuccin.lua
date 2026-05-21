932fab2b12301d228a7a303f36023bc131055b06
Jonghoon Yoon <rhagodessa@gmail.com>
2026-05-21 21:12:24 +0900

    Update nvim config

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -6,6 +6,12 @@ return {
     opts = {
       flavour = "mocha",
       no_italic = true,
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.overlay1 },
+          CursorLineNr = { fg = colors.mauve, style = { "bold" } },
+        }
+      end,
     },
   },
 }
